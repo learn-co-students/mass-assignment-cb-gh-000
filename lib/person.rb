3bfb265472921e47840c892ec7799dbb4a5d8c1e
Ilya Lavandoso <ultraprolid@gmail.com>
2017-03-29 22:12:26 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
 class Person
   #your code here
+  def self.to_attr key
+    attr_accessor key.to_sym
+  end
+
+  def initialize attributes
+    attributes.each do |key,value|
+      self.class.to_attr key
+      self.send(("#{key}="),value)
+    end
+  end
+
 end
